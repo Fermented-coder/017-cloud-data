5976326e46c78b54d97d254682c48b6b51423d25
Kasim <kasim2560@gmail.com>
2024-06-23 17:13:03 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      sm: "3640px",
+      sm: "364px",
       md: "768px",
       lg: "1024px",
       xl: "1280px",
